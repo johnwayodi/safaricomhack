098332268592f23d6bd7357805cbba9715787138
johnwayodi <wayodijohn@gmail.com>
2018-08-04 10:24:55 +0300

    initial commit

---
+++ src/main/java/com/johnwayodi/careerRegistration/CareerRegistrationApplication.java
@@ -0,0 +1,12 @@
+package com.johnwayodi.careerRegistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerRegistrationApplication.class, args);
+	}
+}
